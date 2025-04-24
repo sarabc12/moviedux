0c7408e36d4632692965eca42a9298ffbde5ab48
sara burchi colonna <burchicolonnasara@gmail.com>
2025-04-24 19:02:51 +0200

    new things done

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,23 +5,36 @@ import MovieCard from "./MovieCard";
 export default function MoviesGrid() {
 
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-
     fetch("movies.json")
     .then(response => response.json())
     .then(data => setMovies(data))
-
-
   }, []);
 
+  const handleSearchChange = (e) => {
+   setSearchTerm(e.target.value)
+  };
+
+  const filteredMovies = movies.filter(movie =>
+    movie.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+  );
+
   return(
-    <div className="movies-grid">
-      {
-        movies.map(movie => (
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search movies..."
+        value= {searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div className="movies-grid">
+        {filteredMovies.map(movie => (
           <MovieCard movie={movie} key={movie.id}></MovieCard>
-        ))
-      }
+        ))}
+      </div>
     </div>
   );
 }
